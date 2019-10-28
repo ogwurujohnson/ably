120193428f41f9fbdbb434c0908dfffa04948892
ogwurujohnson <ogwurujohnson@gmail.com>
2019-10-28 02:49:08 +0100

    create the fetch data method for fetching users location

--- src/App.vue
+++ src/App.vue
@@ -92,7 +92,33 @@ export default {
       userlocation: []
     };
   },
+    fetchData() {
+      if (!("geolocation" in navigator)) {
+        this.errorStr = "Geolocation is not available.";
+        return;
 }
+      this.gettingLocation = true;
+      navigator.geolocation.watchPosition(
+        pos => {
+          this.gettingLocation = false;
+          this.initialPosition.lat = pos.coords.latitude;
+          this.initialPosition.lng = pos.coords.longitude;
+          const userData = {
+            position: {
+              lat: pos.coords.latitude,
+              lng: pos.coords.longitude
+            },
+            userName: this.usersName
+          };
+          this.userlocation = userData;
+          this.updateRoom(userData);
+        },
+        err => {
+          this.gettingLocation = false;
+          this.errorStr = err.message;
+        }
+      );
+    },
 </script>
 
 <style>
