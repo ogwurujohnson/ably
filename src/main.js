57211b60450f05ebfe827f83b19425476afd6bde
ogwurujohnson <ogwurujohnson@gmail.com>
2019-10-28 02:02:44 +0100

    initialize map

--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 
+import * as VueGoogleMaps from 'vue2-google-maps'
+ 
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyDLohn9qiin5ypFDHKO8hOGra2V-kvjkvg',
+    libraries: 'places', // This is required if you use the Autocomplete plugin
+    
+  },
+})
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
