41a87ed77cf4b1ed456492d4d7e2444e5af3cde3
ogwurujohnson <ogwurujohnson@gmail.com>
2019-10-28 02:02:56 +0100

    add map to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
-  
+  <div id="app">
+    <GmapMap
+      :center="{lat: 10, lng:10}"
+      :zoom="15"
+      map-type-id="terrain"
+      style="width: 100%; height: 100%"
+    >
+    </GmapMap>
+  </div>
 </template>
 
 <script>
