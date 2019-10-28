dfe48e32ee96477334210819d077f474f17f9481
ogwurujohnson <ogwurujohnson@gmail.com>
2019-10-28 02:46:56 +0100

    add google map

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
   <div id="app">
     <GmapMap
-      :center="{lat: 10, lng:10}"
-      :zoom="15"
+      :center="{lat: initialPosition.lat, lng:initialPosition.lng}"
+      :zoom="10"
       map-type-id="terrain"
       style="width: 100%; height: 100%"
     >
+      <!-- check if icon link in makers payload then display -->
+      <GmapMarker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        :clickable="true"
+        :draggable="false"
+        @click="center=m.position"
+        :icon="m.icon"
+        :title="m.userName"
+      />
     </GmapMap>
   </div>
 </template>
